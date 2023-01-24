3f4f7581825f3ee4a3e08609c52d751396d09b85
Feyi Akomolafe <mofeyifoluwa.akomolafe@mths.ca>
2023-01-24 01:30:34 +0000

    all new files

---
+++ lesson-01/main.c
@@ -0,0 +1,12 @@
+// Copyright (c) 2023 Feyi Akomolafe All rights reserved
+
+// Created by: Feyi Akomolafe
+// Created on: January 2023
+// This program is the Space Aliens game for the GameBoy
+
+#include <gb/gb.h>
+#include <stdio.h>
+
+void main() {
+    printf("Hello, World!");
+}
